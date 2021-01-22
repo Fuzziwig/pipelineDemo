a66d5f9dd11c8809b1f9cccb96d00f0b80d08cf7
Jakob Madsen <Fuzziwig@programmer.net>
2021-01-22 13:06:17 +0100

    v2 commentary

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    //index mapping
     @GetMapping("/")
     public String index(){
         return "index";
